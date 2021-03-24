99ecde94a02a811d00629f0335344b8c7a7a7353
dgc23 <dgc23@hotmail.com>
2021-03-23 20:42:40 -0400

    Update Patch_ESXi_Host.ps1

--- VMware/Patch_ESXi_Host.ps1
+++ VMware/Patch_ESXi_Host.ps1
@@ -1,32 +1,44 @@
-
 # This script will patch ESXi Host based on cluster.
 
-Write-Host "Provide cluster name"
+Write-Host "Please Provide cluster name"
 $c = Read-Host
 $i = Get-Cluster -Name $c |Get-VMHost
 
 
+Get-Cluster -Name $c |get-vm |where {$_.ExtensionData.Runtime.ToolsInstallerMounted -eq $true} |Dismount-Tools
+
 
 foreach ($j in $i){
 
+if ((Get-Compliance -Entity $j.name -ComplianceStatus NotCompliant).Baseline.Name -eq $null){
+Write-Host "No patchse needed for" $j -ForegroundColor Cyan
+}else{
+Write-Host "Starting to patch"$j.Name -ForegroundColor Green
 
-Write-Host $j.Name
+#Checking current host connection status.
+$vmh = Get-VMHost -Name $j.Name |select ConnectionState
+Write-Host "Current Host State:" $j.ConnectionState
 
+#Place Host into Maintence Mode if needed.
+if ((Get-VMHost -Name $j.Name |select ConnectionState) -eq 'Connected'){
 Write-Host "Placing Host" $ESXiHost "into Maintence Mode." -ForegroundColor Green
-Get-VMHost -Name $j.Name |Set-VMHost -State Maintenance -WhatIf
-
-Write-Host "Deploying Patches to host" $j "."
-$BL = (Get-Compliance -Entity $j.name -ComplianceStatus NotCompliant).Baseline.Name
-#Get-Compliance -Entity $j.name -ComplianceStatus NotCompliant -OutVariable
-
-#Get-Baseline -Name $BL.Baseline |
-Write-Host 'baseline' $BL
-Remediate-Inventory -Entity $j.Name -Baseline $BL -ErrorAction SilentlyContinue -Confirm: $false -WhatIf
-
-#Get-Inventory -Name $j.Name |Test-Compliance -UpdateType HostPatch
-#Get-Inventory $j.Name |Test-Compliance -UpdateType HostPatch
-
-Get-VMHost -Name $j.Name |Set-VMHost -State Connected -WhatIf
-#$PatchStat = Get-Inventory $j.Name |Get-Compliance -ComplianceStatus NotCompliant |select baseline
-#Write-Host $PatchStat
+Get-VMHost -Name $j.Name |Set-VMHost -State Maintenance
 }
+
+if((Get-VMHost -Name $j.Name |select ConnectionState) -ne 'Maintenance'){
+#Deploy Patches to the host.
+Write-Host "Deploying Patches to host" $j "."
+$B = Get-Compliance -Entity $j.Name -ComplianceStatus NotCompliant
+Remediate-Inventory -Entity $j.Name -Baseline $B.Baseline -ClusterDisableHighAvailability:$true -ErrorAction SilentlyContinue -Confirm: $false
+}else{
+Write-Host "Could not Remediate Host:" $j.Name "Current Connection State:" $j.ConnectionState
+}
+
+#Return host to other mode.
+if ($vmh.ConnectionState -eq 'Connected'){
+Write-Host "Removing host " $j "from Maintence Mode."
+Get-VMHost -Name $j.Name |Set-VMHost -State $vmh.ConnectionState
+}
+
+}}
+
