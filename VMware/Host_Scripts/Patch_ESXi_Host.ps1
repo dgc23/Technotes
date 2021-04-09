e1a9f67a44def2ab977d5d6d1e27b017fafe4233
dgc23 <dgc23@hotmail.com>
2021-04-08 21:05:34 -0400

    Update Patch_ESXi_Host.ps1

--- VMware/Host_Scripts/Patch_ESXi_Host.ps1
+++ VMware/Host_Scripts/Patch_ESXi_Host.ps1
@@ -1,8 +1,88 @@
 # This script will patch ESXi Host based on cluster.
 
-Write-Host "Please Provide cluster name"
-$c = Read-Host
-$i = Get-Cluster -Name $c |Get-VMHost
+#This loads .NET framework classes
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$vmCluster = get-cluster
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Select a Cluster Name'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
+
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Location = New-Object System.Drawing.Point(75,120)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $okButton
+$form.Controls.Add($okButton)
+
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Location = New-Object System.Drawing.Point(150,120)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $cancelButton
+$form.Controls.Add($cancelButton)
+
+
+
+$listBox = New-Object System.Windows.Forms.ListBox
+$listBox.Location = New-Object System.Drawing.Point(10,40)
+$listBox.Size = New-Object System.Drawing.Size(260,20)
+$listBox.Height = 80
+
+[void] $listBox.Name = $vmCluster
+#[void] $listBox.Items.Add('atl-dc-001')
+#[void] $listBox.Items.Add('atl-dc-002')
+#[void] $listBox.Items.Add('atl-dc-003')
+#[void] $listBox.Items.Add('atl-dc-004')
+#[void] $listBox.Items.Add('atl-dc-005')
+#[void] $listBox.Items.Add('atl-dc-006')
+#[void] $listBox.Items.Add('atl-dc-007')
+
+$form.Controls.Add($listBox)
+
+$form.Topmost = $true
+
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $x = $listBox.SelectedItem
+    $x
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Not needed if dropdown menu works.
+#Write-Host "Please Provide cluster name"
+#$c = Read-Host
+#$i = Get-Cluster -Name $c |Get-VMHost
 
 
 Get-Cluster -Name $c |get-vm |where {$_.ExtensionData.Runtime.ToolsInstallerMounted -eq $true} |Dismount-Tools
@@ -41,4 +121,3 @@ Get-VMHost -Name $j.Name |Set-VMHost -State $vmh.ConnectionState
 }
 
 }}
-
