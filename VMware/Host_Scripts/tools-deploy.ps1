37ee6fb1058c83294e170320ff9d3b6ff5d4dd0a
dgc23 <dgc23@hotmail.com>
2023-04-28 10:28:00 -0400

    Update tools-deploy.ps1

--- VMware/Host_Scripts/tools-deploy.ps1
+++ VMware/Host_Scripts/tools-deploy.ps1
@@ -1,7 +1,8 @@
-﻿Write-Host "This script should be used to deolpy new version of VMtools to the ESXi host using the baseline VMware Tools 12.2 - Host Deploy.  This script will not place host in maintance mode or trigger a reboot of the host."
+Write-Host "This script should be used to deolpy new version of VMtools to the ESXi host using the baseline VMware Tools 12.2 - Host Deploy.  This script will not place host in maintance mode or trigger a reboot of the host."
 $VC = read-host "Please input vCenter FQDN"   
 
 if($VC -ne $null -and $VC -ne ""){
+    Disconnect-VIServer -Server *
     Connect-VIServer -Server $VC
 }
 
@@ -21,15 +22,15 @@ do {
 
     switch ($choice) {
         "1" {
-$clustername = Read-Host "Please type in name of VMhost Cluster"
-$h = Get-Cluster $clustername |Get-VMHost
-$b = Get-Baseline |where name -like 'VMware Tools 12.2 - Host Deploy'
-foreach($i in $h) {
-	if(($i.ConnectionState -eq 'Connected') -or ($i.ConnectionState -eq 'Maintenance')){
-    if($(Get-Baseline -Id $b.Id |Get-Compliance -Entity $i).status -like 'NotCompliant'){
-Write-Host "Updating tools on host " $i
-Get-Baseline -Id $b.Id |Remediate-Inventory -Entity $i.name -Confirm:$false
-}else{
+        $clustername = Read-Host "Please type in name of VMhost Cluster"
+        $h = Get-Cluster $clustername |Get-VMHost
+        $b = Get-Baseline |where name -like 'VMware Tools 12.2 - Host Deploy'
+            foreach($i in $h) {
+	        if(($i.ConnectionState -eq 'Connected') -or ($i.ConnectionState -eq 'Maintenance')){
+            if($(Get-Baseline -Id $b.Id |Get-Compliance -Entity $i).status -like 'NotCompliant'){
+            Write-Host "Updating tools on host " $i
+            Get-Baseline -Id $b.Id |Remediate-Inventory -Entity $i.name -Confirm:$false
+        }else{
 Write-Host "Tools already up to date on host " $i -ForegroundColor Green
 }}else{
 Write-Host "Could not update tools on host " $i.Name "with status of " $i.ConnectionState -ForegroundColor Red
@@ -38,7 +39,7 @@ Write-Host "Could not update tools on host " $i.Name "with status of " $i.Connec
 
 "2" {
     # Code to run for Option 2
-    Write-Host "You chose Option 2"
+    Write-Host "Upgrading VM tools on all hosts attached to the connected vCenter"
     $h = Get-VMHost -Name *
     $b = Get-Baseline |where name -like 'VMware Tools 12.2 - Host Deploy'
     foreach($i in $h) {
